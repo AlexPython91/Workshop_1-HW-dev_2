aa91881f0f6e6480e009110b1bf42429539792fb
alexhvorostov <hvorostov.alex1@gmail.com>
2021-11-05 10:09:40 +0400

    added arrayC numbers more 8

--- dev_2/Program.cs
+++ dev_2/Program.cs
@@ -9,14 +9,11 @@
 
 // Создаем метод заполнения массива
 
-int minValue = new Random().Next(-40, 10);  // создаем min
-int maxValue = new Random().Next(10, 40);   // создаем max
-
-Console.WriteLine("Минимельное значение = " + minValue);
-Console.WriteLine("Максимальное значение = " + maxValue);
-
 int[] arrayA = new int[10];     // создаем массив А 
 int[] arrayB = new int[10];     // создаем массив В
+
+int minValue = new Random().Next(-40, 10);  // создаем min
+int maxValue = new Random().Next(10, 40);   // создаем max
 
 
 for(int j=0; j<10; ++j)   // Заполнение массива случайными числами
@@ -38,19 +35,9 @@ void PrintArray(int[] col)      // метод вывода массива
             position++;
     }  
 }
+Console.WriteLine("Массив: ");
 PrintArray(arrayA);       // вызов метода
 Console.WriteLine();
-
-
-/*for(int j=0; j<10; ++j)   // Заполнение массива случайными числами
-{
-    Random rnd;
-    rnd=new Random();
-    arrayA[j] = new Random().Next(minValue,maxValue);
-} 
-
-PrintArray(arrayA);
-Console.WriteLine();*/
 
 
 int[] createArrayB(int[] arrayB) //Создание массива B на основе массива А с отбрасыванием чисел, нарушающих порядок возрастания
@@ -67,11 +54,36 @@ int[] createArrayB(int[] arrayB) //Создание массива B на осн
                 arrayB[index2] = currentElement;
                 index2++;
             }
-        index++;
+            index++;
     }
     return arrayB;
 }
-
+Console.WriteLine("Числа нарушающие порядок: ");
 arrayB = createArrayB(arrayB);
 PrintArray(arrayB);
 Console.WriteLine();
+
+
+int[] createArrayC(int[] arrayC)     //Создание массива C на основе массива B (откидываем числа больше 8)
+{
+    int len = (arrayB.Length)-1;
+    int index = 0;
+    int index3 = 0;
+    int currentElement2 = -40;
+    int ElementOfarray = 8;
+    while(index <= len)
+    {
+        if(arrayC[index] == ElementOfarray)
+        {
+            currentElement2 = arrayB[index];
+            arrayC[index3] = currentElement2;
+            index3++;
+        }
+        index++;
+    }
+    return arrayC;
+}
+
+Console.WriteLine("Числа нарушающие порядок: ");
+createArrayC(arrayC);
+ 
